3fff62f633f6a478225e4ec27a884287849f6a14
samuluru28 <sandhyaa.7089@gmail.com>
2021-01-18 07:37:22 -0600

    initial commit

---
+++ calculator-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+
+const routes: Routes = [
+  
+  {path: 'calculator', component: CalculatorComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
